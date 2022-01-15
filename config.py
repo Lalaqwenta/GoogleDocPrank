7a5fc050df05cd12dabd2ba429b05fd61973ad5f
Mikle <plmisha97@gmail.com>
2022-01-15 13:44:41 +0300

    Made some minor improvments

---
+++ config.py
@@ -0,0 +1,2 @@
+url = 'https://docs.google.com/forms/d/e/1FAIpQLSdGRAnwy6oHuIua_EWVlwSqFlOfUze0bZRk2WyuuUyLiZad9Q'
+user_agent = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36'
--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # entry.825455790 - милфы начинаются с
 # shagapov@phystech.edu
 
-
+import config
 import requests
 import mimesis
 import locale
@@ -15,22 +15,7 @@ from mimesis.locales import Locale
 from mimesis import locales
 import random
 
-pers = Person(locale=Locale.RU)
-
-GoogleURL = 'https://docs.google.com/forms/d/e/1FAIpQLSdGRAnwy6oHuIua_EWVlwSqFlOfUze0bZRk2WyuuUyLiZad9Q'
-urlResponse = GoogleURL+'/formResponse'
-urlReferer = GoogleURL+'/viewform'
-
-form_data = {
-            'entry.2014055157' : 'Артем слился',
-            'entry.496536405' : {'Анус собаки','Ролевую игру'},
-            'entry.457997219' : 'Геи пидоры из далекого долгопрудного',
-            'entry.825455790' : '18',
-            'emailAddress':'shagapov@phystech.edu'}
-
-user_agent = {'Referer':urlReferer,'User-Agent': "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36"}
-
-for i in range(1, 11):
+def post_record():
     name = pers.full_name()
     email = pers.email(domains=['mail.ru', 'yandex.ru', 'list.ru', 'gmail.com', 'bk.ru'])
     print(f'{name}, {email}')
@@ -39,6 +24,22 @@ for i in range(1, 11):
         'entry.496536405': {'Анус собаки', 'Ролевую игру'},
         'entry.457997219': 'Геи пидоры из далекого долгопрудного',
         'entry.825455790': '18',
-        'emailAddress': email}
+        'emailAddress': email
+    }
     response = requests.post(urlResponse, data=form_data, headers=user_agent)
-    print(response)
+    if response.status_code != 200:
+        print(response)
+
+pers = Person(locale=Locale.RU)
+
+url = config.url
+urlResponse = url+'/formResponse'
+urlReferer = url+'/viewform'
+
+user_agent = {'Referer':urlReferer,'User-Agent': config.user_agent}
+
+def main():
+    for i in range(1, 11):
+        post_record()
+
+main()
