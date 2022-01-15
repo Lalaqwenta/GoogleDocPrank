f426bf9b00c7a20cc54dddf879924c48d053610d
Mikle <plmisha97@gmail.com>
2022-01-15 19:51:06 +0300

    Found some mistakes

--- run.py
+++ run.py
@@ -52,11 +52,11 @@ def post_record():
 #имя
     name = pers.full_name()
 #Во что ты любишь играть
-    what_do_you_like_to_play = multi_select_option_from_file(maps + "whatDoYouLikeToPlay")
+    what_do_you_like_to_play = multi_select_option_from_file(maps + "whatDoYouLikeToPlay", None)
 #Какие проблемы вам встретились
-    what_problems_found = mono_select_option_from_file(maps + "whatProblemsYouFound", None
+    what_problems_found = mono_select_option_from_file(maps + "whatProblemsYouFound", None)
 #Какие проблемы вы решили
-    what_problems_solved = mono_select_option_from_file(maps + "whatProblemsYouSolved", None
+    what_problems_solved = mono_select_option_from_file(maps + "whatProblemsYouSolved", None)
 #На что ты любишь кататься (читаем из файлов)
     what_type_of_rp = multi_select_option_from_file(maps + "whatTypeOfRPAreYou", "add_other")
     what_type_of_rp_other = mono_select_option_from_file(maps + "whatTypeOfRPAreYou_other", None)
@@ -70,37 +70,42 @@ def post_record():
     email = pers.email(domains=['mail.ru', 'yandex.ru', 'list.ru', 'gmail.com', 'bk.ru'])
 #Ты состоишь в каком-нибудь клубе или команде?
     are_you_in_club = mono_select_option_from_file(maps + "areYouInClub", "add_other")
-    are_you_in_club_other = mono_select_option_from_file(maps + "areYouInClub_other", 0)
+    are_you_in_club_other = mono_select_option_from_file(maps + "areYouInClub_other", None)
 #Название клуба в котором состоишь
-    club_name = mono_select_option_from_file(maps + "clubNames", 0)
+    club_name = mono_select_option_from_file(maps + "clubNames", None)
 #Как решил эти проблемы
     how_did_you_solve = multi_select_option_from_file(maps + "howDidYouSolve", "add_other")
-    how_did_you_solve_other = mono_select_option_from_file(maps + "howDidYouSolve_other", 0)
+    how_did_you_solve_other = mono_select_option_from_file(maps + "howDidYouSolve_other", None)
 #Как обрастаешь снарягой
     how_do_you_get_stuff = mono_select_option_from_file(maps + "howDoYouGetStuff", "add_other")
-    how_do_you_get_stuff_other = mono_select_option_from_file(maps + "howDoYouGetStuff_other", 0)
+    how_do_you_get_stuff_other = mono_select_option_from_file(maps + "howDoYouGetStuff_other", None)
 #Как давно в РД
     how_long_in_larp = mono_select_option_from_file(maps + "howLongInLARP", "add_other")
-    how_long_in_larp_other = mono_select_option_from_file(maps + "howLongInLARP_other", 0)
+    how_long_in_larp_other = mono_select_option_from_file(maps + "howLongInLARP_other", None)
 #Сколько игр посетил
-    how_many_games_attended = mono_select_option_from_file(maps + "howManyGamesAttended", 0)
+    how_many_games_attended = mono_select_option_from_file(maps + "howManyGamesAttended", None)
 #Погоняло
-    nickname = mono_select_option_from_file(maps + "nicknames", 0)
+    nickname = mono_select_option_from_file(maps + "nicknames", None)
     nickname = random.choice([nickname, name])
+#
+    what_troubles = mono_select_option_from_file(maps + "whatTroubles", None)
+    what_can_improve = mono_select_option_from_file(maps + "whatCanImprove", None)
+    what_newbie = mono_select_option_from_file(maps + "whatNewbie", None)
+    what_i_like = mono_select_option_from_file(maps + "whatILike", None)
 #формируем json
     form_data = {
         'entry.1226550788' : are_you_in_club,
         'entry.1226550788.other_option_response' : are_you_in_club_other,
         'entry.1949553758' : club_name,
-        'entry.337047416' : how_did_you_solve,
+        'entry.337047416'  : how_did_you_solve,
         'entry.337047416.other_option_response' : how_did_you_solve_other,
-        'entry.522066257' : how_do_you_get_stuff,
+        'entry.522066257'  : how_do_you_get_stuff,
         'entry.522066257.other_option_response' : how_do_you_get_stuff_other,
-        'entry.206608746' : how_long_in_larp,
+        'entry.206608746'  : how_long_in_larp,
         'entry.206608746.other_option_response' : how_long_in_larp_other,
         'entry.1861355305' : how_many_games_attended,
-        'entry.541429037' : nickname,
-        'emailAddress': email,
+        'entry.541429037'  : nickname,
+        'emailAddress'     : email,
         "entry.1315330665" : where_did_you_learn, 
         "entry.1315330665.other_option_response" : where_did_you_learn_other,
         "entry.1741486870" : what_type_of_rp,
@@ -110,14 +115,19 @@ def post_record():
         "entry.265185965"  : what_do_you_like_to_play,
         "entry.650149429"  : what_problems_found,
         "entry.1793078507" : what_problems_solved,
+        "entry.1772746954" : what_troubles,
+        "entry.56781382"   : what_can_improve,
+        "entry.1418118175" : what_newbie,
+        "entry.824422351"  : what_i_like
         }
     print(form_data)
 #сохраняем сгенерированные имена в файл
     save_to_file_by_lines(str(form_data), "logs")
 #отправляем запрос
     response = requests.post(urlResponse, data=form_data, headers=user_agent)
-    if response.status_code != 200:
-        print(response)
+    #if response.status_code != 200:
+        with codecs.open("error" + time.strftime("%Y.%m.%d-%H:%M:%S") + ".html", "a", encoding='utf-8') as file:
+          file.write(response.text + '\n')
 
 # Общие настройки
 pers = Person(locale=Locale.RU)
@@ -131,7 +141,6 @@ user_agent = {'Referer':urlReferer,'User-Agent': config.user_agent}
 def main():
     for i in range(1, 2):
         post_record()
-        random.randint(600, 3600)
-        time.sleep()
+        time.sleep(random.randint(0, 1))
 
 main()
